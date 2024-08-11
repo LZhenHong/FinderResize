66d73db318ebeb78aeadceb65befdf4909ed894b
Eden <levine5blue@gmail.com>
2024-08-12 01:38:48 +0800

    [UPDATE] Support cmd+w shortcuts.

--- FinderResize/FinderResize/AppDelegate.swift
+++ FinderResize/FinderResize/AppDelegate.swift
@@ -8,6 +8,10 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        populateMainMenu()
+    }
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         AXUtils.checkIsTrusted()
         MenuBarItemController.shared.setUp()
@@ -21,5 +25,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidBecomeActive(_ notification: Notification) {
         AXUtils.checkTrustStatus()
+    }
+}
+
+extension AppDelegate {
+    func populateMainMenu() {
+        let mainMenu = NSMenu(title: "Main Menu")
+        let fileMenuItem = mainMenu.addItem(withTitle: "File", action: nil, keyEquivalent: "")
+        let submenu = NSMenu(title: String(localized: "File"))
+
+        let closeWindowItem = NSMenuItem(title: String(localized: "Close Window"),
+                                         action: #selector(NSWindow.performClose(_:)),
+                                         keyEquivalent: "w")
+        submenu.addItem(closeWindowItem)
+
+        mainMenu.setSubmenu(submenu, for: fileMenuItem)
+
+        NSApp.mainMenu = mainMenu
     }
 }
